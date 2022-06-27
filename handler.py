f5c32b3991b058670dd77fb9bd194a7ac86c6c96
iaaanz <iangrasman@gmail.com>
2022-06-27 09:29:03 -0300

    17995 - ajusta path de upload

--- handler.py
+++ handler.py
@@ -9,6 +9,8 @@ from urllib.parse import urlparse
 INVENTORY_REPORT_NAME = 'Registro de Inventário'
 bucket_s3 = 'compufour'
 PDF_PATH = '/tmp/out.pdf'
+homolog_path = 'homolog/uploads/reports/report/'
+production_path = 'production/uploads/reports/report/'
 
 dotenv.load_dotenv()
 
@@ -18,27 +20,34 @@ if os.environ['ENVIRONMENT'] in ['production', 'homolog']:
 else:
     PDFKIT_CONFIG = pdfkit.configuration()
 
-def lambda_info(e): 
+
+def lambda_info(e):
     print('> ', e)
+
 
 def lambda_error(e):
     print('> ERROR: ', e)
+
 
 def upload_file(pdf_path: str):
     '''Faz upload para S3'''
     s3_client = boto3.resource('s3')
     file_name = os.path.basename(pdf_path)
-    bucket_path = 'homolog/uploads/reports/report/' + file_name
+    reports_path = production_path if os.environ[
+        'ENVIRONMENT'] == 'production' else homolog_path
+    bucket_path = reports_path + file_name
     pdf_url = 'https://compufour.s3.amazonaws.com/' + bucket_path
 
     try:
         lambda_info('Enviando PDF para S3')
-        s3_client.meta.client.upload_file(pdf_path, bucket_s3, bucket_path, ExtraArgs={'ACL':'public-read'})
+        s3_client.meta.client.upload_file(
+            pdf_path, bucket_s3, bucket_path, ExtraArgs={'ACL': 'public-read'})
         lambda_info('PDF enviado!')
         lambda_info(pdf_url)
         return pdf_url
     except Exception as e:
         lambda_error(e)
+
 
 def format_response(response) -> dict:
     '''Formata a resposta HTTP'''
@@ -46,11 +55,12 @@ def format_response(response) -> dict:
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/pdf",
-            "Access-Control-Allow-Origin" : "*"
+            "Access-Control-Allow-Origin": "*"
         },
         "body": response,
         "isBase64Encoded": True
     }
+
 
 def format_response_url(response) -> dict:
     '''Formata a resposta HTTP'''
@@ -58,19 +68,20 @@ def format_response_url(response) -> dict:
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin" : "*"
+            "Access-Control-Allow-Origin": "*"
         },
         "body": json.dumps({
             "pdfUrl": response
         })
     }
 
+
 def format_error_response(e: Exception) -> str:
     return {
         "statusCode": 500,
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin" : "*"
+            "Access-Control-Allow-Origin": "*"
         },
         "body": json.dumps({
             'environment': os.environ['ENVIRONMENT'],
@@ -85,17 +96,19 @@ def convert_from_url(page: str) -> str:
     lambda_info('Iniciando conversão...')
 
     parsed_url = urlparse(page)
-    file_name = os.path.basename(parsed_url.path);
+    file_name = os.path.basename(parsed_url.path)
     formatted_file_name = file_name.split(".", 1)[0]
     pdf_path = '/tmp/' + formatted_file_name + ".pdf"
     pdfkit.from_url(page, pdf_path, configuration=PDFKIT_CONFIG)
     return pdf_path
+
 
 def convert(page: str) -> str:
     '''Converte a página para PDF'''
     pdfkit.from_url(page, PDF_PATH, configuration=PDFKIT_CONFIG)
     text = open(PDF_PATH, 'rb').read()
     return base64.b64encode(text).decode('utf-8')
+
 
 def get(event, _) -> dict:
     '''Trabalha a requisição para converter e responder com HTTP'''
